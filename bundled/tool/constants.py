086ba7ed0f207e4c8931d4b96604d4c688a95866
Earlee <li.chunyu0412@gmail.com>
2023-09-25 17:33:51 +0800

    add a new property 'cellMagics' for supporting notebook cell with python magic function (#40)
    * align runner to server
    
    * feat: add a new property 'cellMagics' for supporting notebook cell with python magic function
    
    * set a default value for list in dict

---
+++ bundled/tool/constants.py
@@ -0,0 +1,18 @@
+# -*- encoding: utf-8 -*-
+'''
+@create_time: 2023/09/25 11:17:10
+@author: lichunyu
+'''
+
+
+PYTHON_CELL_MAGICS = frozenset(
+    (
+        "capture",
+        "prun",
+        "pypy",
+        "python",
+        "python3",
+        "time",
+        "timeit",
+    )
+)
