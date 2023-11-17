7e68763bd3b894b9546c1c8d7dd59b73a2c2e0f8
Earlee <li.chunyu0412@gmail.com>
2023-11-17 16:42:31 +0800

    Test range format (#90)
    * change style to pyproject
    
    * delete test scheduler
    
    * test: range format

--- src/test/python_tests/test_server.py
+++ src/test/python_tests/test_server.py
@@ -8,7 +8,7 @@ from hamcrest import assert_that, is_
 from .lsp_test_client import constants, session, utils
 
 
-def test_formatting_example():
+def test_formatting():
     """Test formatting a python file."""
     FORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample1" / "sample.py"
     UNFORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample1" / "sample.unformatted"
@@ -42,14 +42,77 @@ def test_formatting_example():
                 },
             })
 
-    expected = [
-        {
-            "range": {
-                "start": {"line": 0, "character": 0},
-                "end": {"line": len(lines), "character": 0},
+    expected = [{
+        "range": {
+            "start": {
+                "line": 0,
+                "character": 0
             },
-            "newText": FORMATTED_TEST_FILE_PATH.read_text(),
-        }
-    ]
+            "end": {
+                "line": len(lines),
+                "character": 0
+            },
+        },
+        "newText": FORMATTED_TEST_FILE_PATH.read_text(),
+    }]
+
+    assert_that(actual, is_(expected))
+
+
+def test_range_formatting():
+    FORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample2" / "sample.py"
+    UNFORMATTED_TEST_FILE_PATH = constants.TEST_DATA / "sample2" / "sample.unformatted"
+
+    contents = UNFORMATTED_TEST_FILE_PATH.read_text()
+    lines = contents.splitlines(keepends=False)
+
+    actual = []
+
+    with utils.PythonFile(contents, UNFORMATTED_TEST_FILE_PATH.parent) as pf:
+        uri = utils.as_uri(str(pf.fullpath))
+
+        with session.LspSession() as ls_session:
+            ls_session.initialize()
+            ls_session.notify_did_open(
+                {"textDocument": {
+                    "uri": uri,
+                    "languageId": "python",
+                    "version": 1,
+                    "text": contents,
+                }})
+
+            actual = ls_session.text_document_range_formatting({
+                "textDocument": {
+                    "uri": uri
+                },
+                "range": {
+                    "start": {
+                        "line": 61,
+                        "character": 0
+                    },
+                    "end": {
+                        "line": 88,
+                        "character": 0
+                    },
+                },
+                "options": {
+                    "tabSize": 4,
+                    "insertSpaces": True
+                },
+            })
+
+    expected = [{
+        "range": {
+            "start": {
+                "line": 0,
+                "character": 0
+            },
+            "end": {
+                "line": len(lines),
+                "character": 0
+            },
+        },
+        "newText": FORMATTED_TEST_FILE_PATH.read_text(),
+    }]
 
     assert_that(actual, is_(expected))
